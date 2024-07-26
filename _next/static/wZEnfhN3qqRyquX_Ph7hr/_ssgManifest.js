1b7f7cb19595652f1a3582124b217eccf27e9dbd
ayanamists <44194981+ayanamists@users.noreply.github.com>
2024-07-26 15:01:56 +0000

    Deploying to gh-pages from @ ayanamists/Panda.js@b094fd1c2f04d7a9baa787d9385b6250c18c76a8 🚀

---
+++ _next/static/wZEnfhN3qqRyquX_Ph7hr/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fblog","\u002F[locale]\u002Ffavorites\u002Fmottos","\u002F[locale]\u002Fposts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
